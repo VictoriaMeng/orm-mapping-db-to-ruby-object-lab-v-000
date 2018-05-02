a8c8db0410afb81eaedcffc2ccb3f57befbd141a
Victoria Meng <nights1ststar@protonmail.com>
2018-05-02 01:37:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,5 +91,13 @@ class Student
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT name
+      FROM students
+      WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, "10").map do |name|
+      self.find_by_name(name)
+    end.first
   end
 end
